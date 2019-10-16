756af98a0f2c5666525d459ec08baf216f556ec3
Simranbal04 <simranbal01@gmail.com>
2019-10-16 08:46:41 -0400

    Initial Commit

---
+++ UserInputDemo-F19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInputDemo-F19
+//
+//  Created by MacStudent on 2019-10-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
